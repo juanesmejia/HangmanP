71c6558720b008cfa9c111348bf1e2100b56f740
Juan Mejia <juanesmejia1220@gmail.com>
2024-08-07 12:04:03 -0400

    initial style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import {useState} from 'react'
+import words from "./wordList.json"
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState("Test")
-  return <h1> Hi</h1>
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessLetter, setGuessLetters] = useState<string[]> ([])
+  console.log(wordToGuess)
+  return (
+    <div
+      style = {{
+        maxWidth: "850px", display: "flex", flexDirection: "column",
+        gap: "2rem", margin: " 0 auto", alignItems: "center"
+      }}
+    >
+      <div>
+        Lose
+        Win
+      </div>
+
+    </div>
+  )
 }
 
 export default App
